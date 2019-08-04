bb4c5ec46c5a147ecb36830d66cb183229501d57
Tobias Denzler <tobias.denzler@sideline.ch>
2019-08-04 15:19:24 +0200

    add curl to image

--- openshift/setup.sh
+++ openshift/setup.sh
@@ -2,3 +2,6 @@
 
 oc new-project ose-healthchecker
 oc new-app --docker-image=openshift-healthchecker:latest
+oc new-build https://github.com/tobiasdenzler/openshift-healthchecker
+oc new-app ose-healthchecker/openshift-healthchecker:latest
+oc expose service openshift-healthchecker
