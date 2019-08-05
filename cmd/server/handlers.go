ad5ef754fdb9e8060c917ff67f8e264dcb6d5c59
Tobias Denzler <tobias.denzler@sideline.ch>
2019-08-05 23:06:11 +0200

    fix path

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -10,10 +10,10 @@ func (app *application) health(w http.ResponseWriter, r *http.Request) {
 
 // Check if all projects have quota.
 func (app *application) checkProjectQuota(w http.ResponseWriter, r *http.Request) {
-	app.runShellCommand("./scripts/projectHasQuota.sh", w, r)
+	app.runShellCommand("scripts/projectHasQuota.sh", w, r)
 }
 
 // Check if Daemonset is running.
 func (app *application) checkDaemonsetRunning(w http.ResponseWriter, r *http.Request) {
-	app.runShellCommand("./scripts/daemonsetIsRunning.sh", w, r)
+	app.runShellCommand("scripts/daemonsetIsRunning.sh", w, r)
 }
