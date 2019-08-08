bfe38f1ae4ad2221825b6c40897bfc86850c3c2f
Tobias Denzler <tobias.denzler@sideline.ch>
2019-08-08 23:24:38 +0200

    fix path

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -17,12 +17,14 @@ func (app *application) routes() *http.ServeMux {
 	// Walk through all scripts and register the files with a handler
 	err := filepath.Walk(app.scriptBase, func(path string, info os.FileInfo, err error) error {
 		// Check if we have a file
-		if !info.IsDir() && !strings.Contains(info.Name(), "..") {
-			// Construct the path of the script
-			scriptPath := "/" + strings.Split(strings.Replace(path, app.scriptBase+"/", "", 1), ".")[0]
-			// Register the path
-			mux.HandleFunc(scriptPath, app.runBashScript)
-			app.infoLog.Printf("registering path %s with script %s", scriptPath, path)
+		if !info.IsDir() {
+			if !strings.Contains(path, "..") {
+				// Construct the path of the script
+				scriptPath := "/" + strings.Split(strings.Replace(path, app.scriptBase+"/", "", 1), ".")[0]
+				// Register the path
+				mux.HandleFunc(scriptPath, app.runBashScript)
+				app.infoLog.Printf("registering path %s with script %s", scriptPath, path)
+			}
 		}
 		return nil
 	})
