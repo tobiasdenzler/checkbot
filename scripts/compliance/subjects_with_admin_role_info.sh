4bb8b0919267862f8e651df5ed5bdcaa698c8747
Andreas Nowak <andreas.nowak@helvetia.ch>
2019-09-20 12:36:41 +0200

    remove for-loop

--- scripts/compliance/subjects_with_admin_role_info.sh
+++ scripts/compliance/subjects_with_admin_role_info.sh
@@ -8,12 +8,8 @@
 set -eux
 
 # Retrieve all subjects with admin role
-SUBJECTS=$(oc get rolebinding --all-namespaces -o json | jq 'def getsubjects(data): data | "subject="+.subjects.kind+","+"name="+.subjects.name+","+"namespace="+.namespace; .items[] | select(.roleRef.name | test("admin")) | getsubjects({"subjects": .subjects[],"namespace": .metadata.namespace})')
-
-for subject in $SUBJECTS
-do
-    # Return the subjects with admin role, tr will strip quotes
-    echo "1|$subject" | tr -d "\""
-done
+oc get rolebinding --all-namespaces -o json \
+  | jq 'def getsubjects(data): data | "1|subject="+.subjects.kind+","+"name="+.subjects.name+","+"namespace="+.namespace; .items[] | select(.roleRef.name | test("admin")) | getsubjects({"subjects": .subjects[],"namespace": .metadata.namespace})' \
+  | tr -d "\""
 
 exit 0
