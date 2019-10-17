d5e8a5261fe01ee04386ecca9a72b4407bb86cb0
Andreas Nowak <andreas.nowak@helvetia.ch>
2019-10-17 14:29:15 +0200

    add compliance script to get all users and groups in privileged sccs

--- scripts/compliance/subjects_with_privileged_scc.sh
+++ scripts/compliance/subjects_with_privileged_scc.sh
@@ -2,12 +2,12 @@
 
 # ACTIVE true
 # TYPE Gauge
-# HELP get all openshift sccs where .allowPrivilegedContainer is set true
+# HELP get all users and groups in openshift sccs where .allowPrivilegedContainer is set true
 # INTERVAL 60
 
 set -eux
 
-# retrieve all sccs with .allowPrivilegedContainer is set true
+# retrieve all users and groups in sccs with .allowPrivilegedContainer is set true
 oc get scc -o go-template='{{range .items}}{{if eq .allowPrivilegedContainer true}}{{$name := .metadata.name}}{{range $user := .users}}1|privileged_scc={{$name}},user={{$user}}{{"\n"}}{{end}}{{range $group := .groups}}1|privileged_scc={{$name}},group={{$group}}{{"\n"}}{{end}}{{end}}{{end}}'
 
 echo "$(date -u +"%s")|lastrun=timestamp"
